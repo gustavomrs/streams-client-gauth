e01db6871abc23ba275b09e2898d4946bfe3e0d4
Gustavo Marques <gustavomarques@Gustavos-MacBook-Pro.local>
2019-07-20 16:24:41 -0300

    PATCH stream / adding modal with react portal

--- src/actions/index.js
+++ src/actions/index.js
@@ -44,10 +44,11 @@ export const fetchStream = id => async dispatch => {
   dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
-export const updateStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.patch(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const deleteStream = id => async dispatch => {
